b9e4f506106798cd8c677257e340cb5fef12108f
Vniteen <niteen@vibeosys.com>
2016-03-16 18:19:26 +0530

    Takeaway source data insert script

---
+++ takeaway-source-insert-script-16-03-2016.sql
@@ -0,0 +1,5 @@
+INSERT INTO `RestaurantDB`.`takeaway_source` (`SourceId`, `SourceName`, `SourceImg`, `Discount`, `Active`) VALUES ('1', 'Zomatto', 'https://b.zmtcdn.com/images/logo/zomato.jpg', '7', '1');
+INSERT INTO `RestaurantDB`.`takeaway_source` (`SourceId`, `SourceName`, `SourceImg`, `Discount`, `Active`) VALUES ('2', 'Swiggy', 'abc.jpg', '5', '1');
+INSERT INTO `RestaurantDB`.`takeaway_source` (`SourceId`, `SourceName`, `SourceImg`, `Discount`, `Active`) VALUES ('3', 'food panda', 'abc.jpg', '9', '1');
+INSERT INTO `RestaurantDB`.`takeaway_source` (`SourceId`, `SourceName`, `SourceImg`, `Discount`, `Active`) VALUES ('4', 'Tiny Owl', 'abc.jpg', '12.5', '1');
+INSERT INTO `RestaurantDB`.`takeaway_source` (`SourceId`, `SourceName`, `SourceImg`, `Discount`, `Active`) VALUES ('5', 'Faasos', 'abc.jpg', '7', '1');
