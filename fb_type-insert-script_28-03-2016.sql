579fa932934816a259d774289ae4456a835b7ef4
Vniteen <niteen@vibeosys.com>
2016-03-28 13:17:10 +0530

    Fb_type-insert scripts

---
+++ fb_type-insert-script_28-03-2016.sql
@@ -0,0 +1,3 @@
+INSERT INTO `RestaurantDB`.`fb_types` (`FbTypeId`, `FbTypeName`, `Active`) VALUES ('1', 'Veg', '1');
+INSERT INTO `RestaurantDB`.`fb_types` (`FbTypeId`, `FbTypeName`, `Active`) VALUES ('2', 'Non-Veg', '1');
+INSERT INTO `RestaurantDB`.`fb_types` (`FbTypeId`, `FbTypeName`, `Active`) VALUES ('3', 'beverages', '1');
