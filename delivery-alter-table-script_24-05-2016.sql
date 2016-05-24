38264fcb6faf627390be1909e17a3fd58e28baca
Vniteen <niteen@vibeosys.com>
2016-05-24 19:16:36 +0530

    Delivery and Takeaway Status

---
+++ delivery-alter-table-script_24-05-2016.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `RestaurantDB`.`delivery` 
+CHANGE COLUMN `Status` `Status` INT(11) NULL DEFAULT 0 COMMENT '' ;
---
+++ takeaway-alter-table-script_24-05-2016.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `RestaurantDB`.`takeaway` 
+CHANGE COLUMN `Status` `Status` INT(11) NULL DEFAULT 0 COMMENT '' ;
